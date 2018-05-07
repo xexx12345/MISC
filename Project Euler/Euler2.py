15b7cd50768d3bc8a818f699a0cb176c9bdb14e0
xexx12345 <chengshihting@gmail.com>
2018-05-06 21:07:31 -0400

    W

--- Project Euler/Euler2.py
+++ Project Euler/Euler2.py
@@ -9,4 +9,13 @@ def fib(n):
         temp = a + b
         a = b
         b = temp
+        
+        
+def fib1(n):
+    a = 1
+    b = 2
+    for i in range(n):
+        yield a,
+        print(a)
+        a, b = b, a + b
 
