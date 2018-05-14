457c3c5e912a6db8fa4d750319e9e7d8a9ff576b
xexx12345 <chengshihting@gmail.com>
2018-05-14 17:08:24 -0400

    W

---
+++ Project Euler/.ipynb_checkpoints/Euler2-checkpoint.py
@@ -0,0 +1,21 @@
+
+#Euler 2
+
+def fib(n):
+    a = 1
+    b = 2
+    for i in range(n):
+        print(a)
+        temp = a + b
+        a = b
+        b = temp
+        
+        
+def fib1(n):
+    a = 1
+    b = 2
+    for i in range(n):
+        yield a,
+        print(a)
+        a, b = b, a + b
+
